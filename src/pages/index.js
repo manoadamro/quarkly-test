6586ecf385d41c002279d6a9504a0269b999a5f4
Adam Romano <manoadamro.imp@gmail.com>
2020-11-20 14:44:12 +0000

    Changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,12 +14,8 @@ export default (() => {
 			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
 		</Helmet>
 		<Components.TestComponent>
-			<Override slot="text1">
-				Hi! I'm a paragraph. Click here to add your own text and edit me. It’s a piece of cake. I’m a great space for you to tell a story and let your site visitors know more about you. Talk about your business and what products and services you offer. Share how you came up with the idea for your company and what makes you different from your competitors. Make your business stand out and show your visitors who you are.
-				<br />
-				<br />
-				<br />
-				changes
+			<Override slot="button">
+				hello
 			</Override>
 		</Components.TestComponent>
 		<Components.Footer />
